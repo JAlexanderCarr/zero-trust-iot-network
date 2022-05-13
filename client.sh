24b328dffda72efecb946f80dc342981d7b1b66f
Alexander Carr <jalexandercarr@gmail.com>
2022-05-13 22:43:11 +0000

    Updated client script

--- client.sh
+++ client.sh
@@ -6,12 +6,12 @@ echo -n "Compiling Client Code ... "
   cd client
   make
   cd ..
-} &> /dev/null
+} > /dev/null
 echo "OK"
 
 # The IP Address of the broker is required to start the client
+cd client
 echo -n "MQTT Broker IP Address: "
 read mqtt_broker
-cd client
-./bin/client.exe $mqtt_broker
+./bin/mqtt_client.exe $mqtt_broker
 cd ..
